41fa813b81d759b4e641ef808097943ffce61ae2
Abo Ellil <mr.abuellil@gmail.com>
2023-04-05 09:12:27 +0200

    Some styling and motion

--- app.js
+++ app.js
@@ -28,17 +28,30 @@ window.onscroll = ()=>{
     }
 
   })
+
+  // ===================== Sticky NavBar ====================
   const header = document.querySelector('header')
 header.classList.toggle('sticky', window.scrollY>100)
+
+
+
 menuIcon.classList.remove('fa-x')
   navbar.classList.remove('active')
 }
 
-// ===================== Sticky NavBar ====================
+// ===================== Scroll reveal ====================
 
+ScrollReveal({
+  reset:true,
+  distance:'80px',
+  duration:2000,
+  delay:200
+})
 
-
-
+ScrollReveal().reveal('.home-content, .heading',{origin:'top'})
+ScrollReveal().reveal('.home-img, .services-container, portfolio-box, .contact form',{origin:'bottom'})
+ScrollReveal().reveal('.about-img, .home-content h1',{origin:'left'})
+ScrollReveal().reveal('.home-content p, .about-content ',{origin:'right'})
 
 
 
