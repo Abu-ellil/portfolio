0054edafee76ce03ea9ff4991b9f1c0035350961
Abo Ellil <mr.abuellil@gmail.com>
2023-04-07 05:26:45 +0200

    44

--- app.js
+++ app.js
@@ -57,23 +57,88 @@ ScrollReveal().reveal('.home-content p, .about-content ',{origin:'right'})
 // =================== certifications ===================
 
 
-let slideIndex = 2; // the index of the middle slide
-showSlides(slideIndex);
+const certifications = [
+  {
+    name: "Certification 3",
+    image:
+      "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/1670172004596.png?raw=true",
+    issuer: "Programming Foundations: Beyond the Fundamentals",
+    date: "December, 2022",
+    link: "https://www.linkedin.com/learning/certificates/7e74ef7e58ab08aa3be9d7ab3cf3cb237a2d9527bb68b87b871905c225e6996b",
+  },
+  {
+    name: "Certification 1",
+    image:
+      "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/code.png?raw=true",
+    issuer: " Build Responsive Real-World Websites with HTML and CSS",
+    date: "December 2022",
+    link: "https://www.udemy.com/certificate/UC-a731ae10-edd2-4291-906b-0b148d8703f0/",
+  },
+  {
+    name: "Certification 2",
+    image:
+      "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/js.png?raw=true",
+    issuer: "JavaScript - The Complete Guide 2023 (Beginner + Advanced)",
+    date: "February 2023",
+    link: "https://www.udemy.com/certificate/UC-11cd8a7a-da8d-4ec4-8dd7-c9545f0edebb/",
+  },
+  {
+    name: "Certification 2",
+    image:
+      "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/reactjs.png?raw=true",
+    issuer: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+    date: "April 2023",
+    link: "https://www.udemy.com/course/react-the-complete-guide-incl-redux",
+  },
+  {
+    name: "Certification 3",
+    image:
+      "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/node.png?raw=true",
+    issuer: "NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)",
+    date: "May 2023",
+    link: "https://www.udemy.com/course/nodejs-the-complete-guide/",
+  },
+];
+      certifications.map((certification) => {
+        const certificationCard = document.createElement("div");
+        certificationCard.classList.add("certification-card");
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+        const certificationCardInner = document.createElement("div");
+        certificationCardInner.classList.add("certification-card-inner");
 
-function showSlides(n) {
-  let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.remove("current");
-  }
-  slides[slideIndex - 1].classList.add("current");
-}
+        const certificationCardFront = document.createElement("div");
+        certificationCardFront.classList.add("certification-card-front");
+
+        const certificationImage = document.createElement("img");
+        certificationImage.src = certification.image;
+        certificationImage.alt = certification.name;
+        certificationCardFront.appendChild(certificationImage);
+
+        const certificationName = document.createElement("h3");
+        certificationName.innerText = certification.name;
+        certificationCardFront.appendChild(certificationName);
+
+        const certificationCardBack = document.createElement("div");
+        certificationCardBack.classList.add("certification-card-back");
+
+        const issuerName = document.createElement("h3");
+        issuerName.innerText = certification.issuer;
+        certificationCardBack.appendChild(issuerName);
+
+        const certificationDate = document.createElement("p");
+        certificationDate.innerText = "Date: " + certification.date;
+        certificationCardBack.appendChild(certificationDate);
+
+        const certificationLink = document.createElement("a");
+        certificationLink.href = certification.link;
+        certificationLink.target = "_blank";
+        certificationLink.rel = "noreferrer";
+        certificationLink.innerText = "View Certificate";
+        certificationCardBack.appendChild(certificationLink);
+
+        certificationCardInner.appendChild(certificationCardFront);
+        certificationCardInner.appendChild(certificationCardBack);
+        certificationCard.appendChild(certificationCardInner);
+        document.querySelector(".certifications-container").appendChild(certificationCard);
+      });
+
